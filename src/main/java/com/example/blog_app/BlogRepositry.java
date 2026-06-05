da2ee2fbcd2eda30d9bba58d297f75d1104d104f
Hirayama <2026028@fsc-educ.com>
2026-06-05 10:59:49 +0900

    0605-1

---
+++ src/main/java/com/example/blog_app/BlogRepositry.java
@@ -0,0 +1,8 @@
+package com.example.blog_app;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BlogRepositry {
+    
+}
--- src/main/java/com/example/blog_app/BookService.java
+++
@@ -1,5 +0,0 @@
-package com.example.blog_app;
-
-public class BookService {
-    
-}
